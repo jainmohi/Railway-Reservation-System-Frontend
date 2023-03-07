bc36ff39e35383dce6d5050f59ff1a3c3d73c6b6
mohi <mohijain15@gmail.com>
2023-03-07 16:43:01 +0530

    aboutus

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,21 +1,25 @@
 function About()
 {
     return <div>
-                <p>
+      <h4>ABOUT US</h4>
+      <br></br>
+            <h5>
+            <p>
             Railway Reservation System is an online platform for booking train tickets, canceling
             tickets, and managing your reservations. We provide a simple and efficient way to book
             your train tickets from the comfort of your home. Our platform is user-friendly and easy
             to navigate, allowing you to book your train tickets quickly and easily.
-          </p>
-          <p>
+            </p>
+            <p>
             Our mission is to provide a hassle-free booking experience for our customers. We are
             committed to providing the best service possible and ensuring that our customers have a
             smooth and enjoyable journey.
-          </p>
-          <p>
+            </p>
+            <p>
             If you have any questions or concerns, please feel free to contact us using the Contact
             Us page on our website.
-          </p>
+            </p>
+            </h5>
             </div>
 }
 
